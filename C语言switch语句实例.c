c1c70f1ccbb0f1b969eef312bf89d9b7b97eebc3
Kevin luo <97839160+2744401789Kevin@users.noreply.github.com>
2022-02-13 22:13:39 +0800

    Add files via upload

---
+++ C语言switch语句实例.c
@@ -0,0 +1,33 @@
+
+#include<stdio.h>
+int main()
+{
+	int day=0;
+	scanf("%d",&day);
+	switch(day)
+	{
+		case 1:
+		printf("星期一");
+		break;
+		case 2:
+		printf("星期二");
+		break;
+		case 3:
+		printf("星期三");
+		break;
+		case 4:
+		printf("星期四");
+		break;
+		case 5:
+		printf("星期五");
+		break;
+		case 6:
+		printf("星期六");
+		break;
+		case 7:
+		printf("星期天");
+		break;
+	}
+	return 0;
+
+}                                   
